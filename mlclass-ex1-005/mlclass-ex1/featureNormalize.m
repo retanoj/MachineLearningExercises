591c871cbadc85cdde738dab8bc1e14cc5eab304
Jonater <s919student@gmail.com>
2014-07-26 15:36:20 +0800

    fix comment

--- mlclass-ex1-005/mlclass-ex1/featureNormalize.m
+++ mlclass-ex1-005/mlclass-ex1/featureNormalize.m
@@ -27,7 +27,7 @@ sigma = zeros(1, size(X, 2));
 %       
 
 for i = 1:size(mu,2)                %对于mu的第i列
-    mu(1, i) = mean( X(:, i));      %计算X的第i列的平均在
+    mu(1, i) = mean( X(:, i));      %计算X的第i列的平均值
     sigma(1, i) = std( X(:, i));    %计算X的第i列的标准差
     X_norm(:, i) = ( X(:, i) - mu(1, i)) / sigma(1, i);
                                     %对X每一行的第i列正规化
